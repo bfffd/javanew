92ef563955a353f30d503db98ea019ccd5000a8a
bfffd <ankitagond030@gmail.com>
2022-11-24 21:57:32 +0530

    second Projrct

---
+++ src/Details/EmplyDetails.java
@@ -0,0 +1,7 @@
+package Details;
+
+public class EmplyDetails {
+public static void main(String[] args) {
+	System.out.println("Name: vishal from talbros auto");
+}
+}
---
+++ src/Details/StudentInfo.java
@@ -0,0 +1,7 @@
+package Details;
+
+public class StudentInfo {
+public static void main(String[] args) {
+	System.out.println("This is vishal");
+}
+}
