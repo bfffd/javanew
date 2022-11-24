3ad22685e3b5699923521b2e2c400a7775999ac9
bfffd <ankitagond030@gmail.com>
2022-11-25 00:55:49 +0530

    updated Project

---
+++ src/Details/EmployeeId.java
@@ -0,0 +1,7 @@
+package Details;
+
+public class EmployeeId {
+public static void main(String[] args) {
+	System.out.println("employee id from 1 to 20");
+}
+}
--- src/Details/EmplyDetails.java
+++ src/Details/EmplyDetails.java
@@ -1,7 +1,12 @@
 package Details;
 
 public class EmplyDetails {
+	void NewAdded()
+	{
+		System.out.println("new addition done");
+	}
 public static void main(String[] args) {
+	
 	System.out.println("Name: vishal from talbros auto");
 }
 }
